1709cf0b876a2776168059dc61fbea210c1a0c69
Scott <scrosehart@gmail.com>
2019-06-01 17:53:08 -0700

    started explore page category sub page and BuddyTile_Large;

---
+++ src/components/BuddyTile_Large.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+import EStyleSheet from 'react-native-extended-stylesheet';
+
+export default function BuddyTile_Large(props) {
+  return (
+    <View style={styles.wrapper}>
+      <Text>I'm BuddyTile_Large</Text>
+    </View>
+  );
+}
+
+const styles = EStyleSheet.create({
+  $size: '$width * 0.7',
+
+  wrapper: {
+    height: '$size',
+    width: '$size',
+    margin: '$size / 12',
+    backgroundColor: 'lightgrey',
+  },
+});
--- src/screens/explore/CategoryScreen.js
+++ src/screens/explore/CategoryScreen.js
@@ -1,6 +1,8 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import { ScrollView, Text, View } from 'react-native';
 import EStyleSheet from 'react-native-extended-stylesheet';
+
+import BuddyTile_Large from '../../components/BuddyTile_Large'
 
 export default class CategoryScreen extends React.Component {
   constructor(props) {
@@ -13,7 +15,48 @@ export default class CategoryScreen extends React.Component {
   render() {
     return (
       <View style={styles.page}>
-        <Text>I'm CategoryScreen</Text>
+
+        <View style={styles.header}>
+          <Text style={[ styles.text, styles.headerText ]}>Header...TECH</Text>
+        </View>
+
+        <Text>Search bar here</Text>
+
+        <ScrollView>
+
+          <Text>Category 1</Text>
+          <ScrollView horizontal>
+            <BuddyTile_Large />
+            <BuddyTile_Large />
+            <BuddyTile_Large />
+            <BuddyTile_Large />
+          </ScrollView>
+
+          <Text>Category 2</Text>
+          <ScrollView horizontal>
+            <BuddyTile_Large />
+            <BuddyTile_Large />
+            <BuddyTile_Large />
+            <BuddyTile_Large />
+          </ScrollView>
+
+          <Text>Category 3</Text>
+          <ScrollView horizontal>
+            <BuddyTile_Large />
+            <BuddyTile_Large />
+            <BuddyTile_Large />
+            <BuddyTile_Large />
+          </ScrollView>
+
+          <Text>Category 4</Text>
+          <ScrollView horizontal>
+            <BuddyTile_Large />
+            <BuddyTile_Large />
+            <BuddyTile_Large />
+            <BuddyTile_Large />
+          </ScrollView>
+
+        </ScrollView>
       </View>
     );
   }
@@ -23,5 +66,19 @@ const styles = EStyleSheet.create({
   page: {
     flex: 1,
     justifyContent: 'center',
+  },
+  header: {
+    height: '$height / 8',
+    paddingBottom: '20rem',
+    justifyContent: 'flex-end',
+    alignItems: 'center',
+    backgroundColor: 'lightsalmon',
+  },
+  text: {
+    //
+  },
+  headerText: {
+    color: '$white',
+    fontSize: '24rem',
   },
 });
